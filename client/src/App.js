bd68eab39efe37f8950c30904705e1c7409df893
Nicholas Geary <ncgeary@buffalo.edu>
2019-03-11 16:00:53 -0400

    Redux implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+
+import {Provider} from 'react-redux';
+import store from './store';
 
 import AppNavbar from './component/navbar/navbar';
 import MainInfo from './component/main_info/info';
-import TableNav from './component/tablenav/tablenav';
 import CurrentTable from './component/currentTable/currentTable';
 
 import Footer from './component/footer/footer';
@@ -11,15 +14,17 @@ import Footer from './component/footer/footer';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-        <MainInfo/>
-        <TableNav/>
-        <CurrentTable/>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavbar />
+          <MainInfo/>
+          <CurrentTable/>
 
-        <Footer/>
+          <Footer/>
 
-      </div>
+        </div>
+      </Provider>
+      
     );
   }
 }
--- client/src/component/currentTable/currentTable.jsx
+++ client/src/component/currentTable/currentTable.jsx
@@ -1,42 +1,58 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux';
+import {getItems} from '../../actions/itemActions';
+import PropTypes from 'prop-types'
 import { 
   BootstrapTable,
   TableHeaderColumn } from 'react-bootstrap-table';
+import { Container } from 'reactstrap';
 
-const nhlconference = {
-  0: 'All NHL',
-  1: 'Eastern',
-  2: 'Western'
-};
 
-function enumFormatter(cell, row, enumObject) {
-  return enumObject[cell];
-}
+// function enumFormatter(cell, row, enumObject) {
+//   return enumObject[cell];
+// 
 
 class CurrentTable extends Component {
+  componentDidMount() {
+    this.props.getItems();
+  }
+
   render() {
+    const {items}= this.props.item;
+
     return (
-      <div>
+      <Container>
+        <BootstrapTable printable data={items} striped hover>
 
-        <BootstrapTable data={products}>
-          <TableHeaderColumn
-            dataField='quality'
-            filterFormatted
-            dataFormat={enumFormatter}
-            formatExtraData={nhlconference}
-            filter={{ type: 'SelectFilter', options: nhlconference }}>Conference</TableHeaderColumn>
+          <TableHeaderColumn dataField='Team' width='250' dataAlign='center' dataSort isKey>Team</TableHeaderColumn>
+          <TableHeaderColumn dataField='Current_Rank' width='150' dataAlign='center'>Current Rank</TableHeaderColumn>
+          <TableHeaderColumn dataField='New_Rank' width='150' dataAlign='center'>321 Rank</TableHeaderColumn>
+          <TableHeaderColumn dataField='Wins' width='75'dataAlign='center'>Wins</TableHeaderColumn>
+          <TableHeaderColumn dataField='Loss' width='75' dataAlign='center'>Loss</TableHeaderColumn>
+          <TableHeaderColumn dataField='Overtime_Loss' width='150' dataAlign='center'>Overtime Loss</TableHeaderColumn>
+          <TableHeaderColumn dataField='Shoot_Out_Wins' width='150' dataAlign='center'>Shoot Out Wins</TableHeaderColumn>
+          <TableHeaderColumn dataField='Overtime_Wins' width='150' dataAlign='center'>Overtime Wins</TableHeaderColumn>
+          <TableHeaderColumn dataField='True_Wins' width='100' dataAlign='center'>True Wins</TableHeaderColumn>
+          <TableHeaderColumn dataField='New_Record' width='150' dataAlign='center'>New Record</TableHeaderColumn>
+          <TableHeaderColumn dataField='Points' width='100' dataAlign='center'>Points</TableHeaderColumn>
+          <TableHeaderColumn dataField='Current_Points' width='150' dataAlign='center'>Current Points</TableHeaderColumn>
 
-          <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
 
-          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-
-          
         </BootstrapTable>
-        
-      </div>
+
+      </Container>
+      
     )
   }
 }
 
+CurrentTable.propTypes = {
+  getItems: PropTypes.func.isRequired,
+  item: PropTypes.object.isRequired
+}
 
-export default CurrentTable;
+const mapStateToProps = (state) => ({
+  item: state.item
+});
+
+export default connect(mapStateToProps,{ getItems })(CurrentTable);
