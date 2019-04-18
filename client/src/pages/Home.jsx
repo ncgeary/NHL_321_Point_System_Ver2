2db8242b9d891e77cbfdcc99ccd9d3ebed6d6362
Nicholas Geary <ncgeary@buffalo.edu>
2019-04-18 14:53:34 -0400

    Adding Router

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
 
 import {Provider} from 'react-redux';
 import store from './store';
@@ -12,9 +8,10 @@ import store from './store';
 import AppNavbar from './component/navbar/navbar';
 import MainInfo from './component/main_info/info';
 import CurrentTable from './component/currentTable/currentTable';
+
 import Footer from './component/footer/footer';
 
-class App extends Component {
+class Home extends Component {
   render() {
     return (
       <Provider store={store}>
@@ -32,4 +29,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Home;
