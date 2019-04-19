cac4fe158edece9c2a748ae82c8879608c8cc72c
Nicholas Geary <ncgeary@buffalo.edu>
2019-04-18 22:23:20 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import {Provider} from 'react-redux';
 import store from './store';
